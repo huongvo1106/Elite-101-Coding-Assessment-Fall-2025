d9a1d23038e98b9c3aa2f47db306335c8b9b7880
huongvo1106 <56045114+huongvo1106@users.noreply.github.com>
2025-11-13 02:16:38 +0000

    level 4

--- level4.py
+++ level4.py
@@ -1,13 +1,26 @@
 import library_books
-from datetime import datetime
+from datetime import datetime, date
 
 #Write a function to return a book by ID. Reset its availability and due date.
 #Write another function to list all overdue books (i.e., books with due_date in the past and still checked out).
 
-def update_book_info(returned_book_id):
+def update_book_info(book_id):
+    for book in library_books.LIBRARY_BOOKS:
+        if book['id'] == book_id:
+            book['available'] = True
+            book['due_date'] = None
     
 
+def get_overdue_books():
+    over_due_books = []
+    today_date = date.today()
+    for book in library_books.LIBRARY_BOOKS:
+        if book['due_date'] != None and book['available'] == False:
+            get_book_due_date = datetime.strptime(book['due_date'],"%Y-%m-%d").date()
+            if get_book_due_date < today_date:
+                over_due_books.append(book['id'])
+    return over_due_books
 
 
-returned_book_id = input("Enter returned book ID: ")
-update_book_info(returned_book_id)
+print(get_overdue_books())
+
